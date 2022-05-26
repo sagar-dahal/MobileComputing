1f0df7f626f562920b9bd0825c37bdb2a0c871e9
unknown <dahal25sagar@gmail.com>
2022-05-25 22:46:33 -0400

    minor updates

--- app/src/main/java/com/example/remindur/DatabaseHandler.java
+++ app/src/main/java/com/example/remindur/DatabaseHandler.java
@@ -14,12 +14,12 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
     private static final String NAME = "taskListDatabase";
-    private static final String TODO_TABLE = "task";
+    private static final String TASK_TABLE = "task";
     private static final String ID = "id";
     private static final String TASK = "task";
     private static final String STATUS = "status";
     private static final String DATE = "date";
-    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, "
+    private static final String CREATE_TASK_TABLE = "CREATE TABLE " + TASK_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, "
             + STATUS + " INTEGER, " + DATE + " TEXT);";
 
     private SQLiteDatabase db;
@@ -30,12 +30,12 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TODO_TABLE);
+        db.execSQL(CREATE_TASK_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TODO_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + TASK_TABLE);
         onCreate(db);
     }
 
@@ -48,7 +48,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         cv.put(TASK, task.getTask());
         cv.put(STATUS, 0);
         cv.put(DATE, task.getDate());
-        db.insert(TODO_TABLE, null, cv);
+        db.insert(TASK_TABLE, null, cv);
     }
 
     public List<TasksModel> getAllTasks(){
@@ -56,7 +56,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         Cursor cur = null;
         db.beginTransaction();
         try{
-            cur = db.query(TODO_TABLE, null, null, null, null, null, null, null);
+            cur = db.query(TASK_TABLE, null, null, null, null, null, null, null);
             if(cur != null){
                 if(cur.moveToFirst()){
                     do{
@@ -82,16 +82,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void updateStatus(int id, int status){
         ContentValues cv = new ContentValues();
         cv.put(STATUS, status);
-        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
+        db.update(TASK_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
     }
 
     public void updateTask(int id, String task) {
         ContentValues cv = new ContentValues();
         cv.put(TASK, task);
-        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
+        db.update(TASK_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
     }
 
     public void deleteTask(int id){
-        db.delete(TODO_TABLE, ID + "= ?", new String[] {String.valueOf(id)});
+        db.delete(TASK_TABLE, ID + "= ?", new String[] {String.valueOf(id)});
     }
 }
